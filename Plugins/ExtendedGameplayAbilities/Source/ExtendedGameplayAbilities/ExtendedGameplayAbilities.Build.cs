b8363a920cf2dbb5341849d2d4d61245cd8e6381
Bohdon Sayre <bohdon@gmail.com>
2023-10-01 23:37:06 -0400

    add plugin source files

---
+++ Plugins/ExtendedGameplayAbilities/Source/ExtendedGameplayAbilities/ExtendedGameplayAbilities.Build.cs
@@ -0,0 +1,27 @@
+﻿// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class ExtendedGameplayAbilities : ModuleRules
+{
+	public ExtendedGameplayAbilities(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[]
+		{
+			"Core",
+			"GameplayAbilities",
+			"GameplayTags",
+			"GameplayTasks",
+		});
+
+		PrivateDependencyModuleNames.AddRange(new string[]
+		{
+			"CoreUObject",
+			"Engine",
+			"Slate",
+			"SlateCore",
+		});
+	}
+}
---
+++ Plugins/ExtendedGameplayAbilities/Source/ExtendedGameplayAbilities/Private/ExtendedGameplayAbilitiesModule.cpp
@@ -0,0 +1,18 @@
+﻿// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ExtendedGameplayAbilitiesModule.h"
+
+#define LOCTEXT_NAMESPACE "FExtendedGameplayAbilitiesModule"
+
+
+void FExtendedGameplayAbilitiesModule::StartupModule()
+{
+}
+
+void FExtendedGameplayAbilitiesModule::ShutdownModule()
+{
+}
+
+#undef LOCTEXT_NAMESPACE
+
+IMPLEMENT_MODULE(FExtendedGameplayAbilitiesModule, ExtendedGameplayAbilities)
