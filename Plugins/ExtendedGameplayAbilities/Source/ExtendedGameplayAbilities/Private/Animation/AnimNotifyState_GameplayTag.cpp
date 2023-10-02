7e5ad4f2cb39bc473eca73bc5b04f0525ed46808
Bohdon Sayre <bohdon@gmail.com>
2023-10-02 01:56:34 -0400

    add gameplay tag anim notify state

---
+++ Plugins/ExtendedGameplayAbilities/Source/ExtendedGameplayAbilities/Private/Animation/AnimNotifyState_GameplayTag.cpp
@@ -0,0 +1,43 @@
+﻿// Copyright Bohdon Sayre, All Rights Reserved.
+
+
+#include "Animation/AnimNotifyState_GameplayTag.h"
+
+#include "AbilitySystemComponent.h"
+#include "AbilitySystemGlobals.h"
+
+
+UAnimNotifyState_GameplayTag::UAnimNotifyState_GameplayTag()
+{
+#if WITH_EDITORONLY_DATA
+	NotifyColor = FColor(100, 255, 225);
+#endif
+}
+
+FString UAnimNotifyState_GameplayTag::GetNotifyName_Implementation() const
+{
+	if (GameplayTag.IsValid())
+	{
+		return FString::Printf(TEXT("Tag: %s"), *GameplayTag.ToString());
+	}
+
+	return Super::GetNotifyName_Implementation();
+}
+
+void UAnimNotifyState_GameplayTag::NotifyBegin(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, float TotalDuration,
+                                               const FAnimNotifyEventReference& EventReference)
+{
+	if (UAbilitySystemComponent* AbilitySystem = UAbilitySystemGlobals::GetAbilitySystemComponentFromActor(MeshComp->GetOwner()))
+	{
+		AbilitySystem->AddLooseGameplayTag(GameplayTag);
+	}
+}
+
+void UAnimNotifyState_GameplayTag::NotifyEnd(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation,
+                                             const FAnimNotifyEventReference& EventReference)
+{
+	if (UAbilitySystemComponent* AbilitySystem = UAbilitySystemGlobals::GetAbilitySystemComponentFromActor(MeshComp->GetOwner()))
+	{
+		AbilitySystem->RemoveLooseGameplayTag(GameplayTag);
+	}
+}
---
+++ Plugins/ExtendedGameplayAbilities/Source/ExtendedGameplayAbilities/Public/Animation/AnimNotifyState_GameplayTag.h
@@ -0,0 +1,33 @@
+﻿// Copyright Bohdon Sayre, All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+#include "Animation/AnimNotifies/AnimNotifyState.h"
+#include "AnimNotifyState_GameplayTag.generated.h"
+
+
+/**
+ * Applies a gameplay tag for the duration of an anim notify.
+ */
+UCLASS(Const, HideCategories = Object, CollapseCategories, Meta = (DisplayName = "Gameplay Tag"))
+class EXTENDEDGAMEPLAYABILITIES_API UAnimNotifyState_GameplayTag : public UAnimNotifyState
+{
+	GENERATED_BODY()
+
+public:
+	UAnimNotifyState_GameplayTag();
+
+	/** The tag to add */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (ExposeOnSpawn = true), Category = "AnimNotify")
+	FGameplayTag GameplayTag;
+
+	virtual FString GetNotifyName_Implementation() const override;
+
+	virtual void NotifyBegin(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, float TotalDuration,
+	                         const FAnimNotifyEventReference& EventReference) override;
+
+	virtual void NotifyEnd(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation,
+	                       const FAnimNotifyEventReference& EventReference) override;
+};
