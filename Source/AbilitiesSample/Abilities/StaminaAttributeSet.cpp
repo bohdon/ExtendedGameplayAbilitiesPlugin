7bdd95b22aa134b0db4e8ab792f92e8e915dab35
Bohdon Sayre <bohdon@gmail.com>
2025-02-21 01:15:58 -0500

    add stamina attribute, make dash cost stamina, fix hud input config

---
+++ Source/AbilitiesSample/Abilities/StaminaAttributeSet.cpp
@@ -0,0 +1,76 @@
+﻿// Copyright Bohdon Sayre, All Rights Reserved.
+
+
+#include "StaminaAttributeSet.h"
+
+#include "Net/UnrealNetwork.h"
+
+UStaminaAttributeSet::UStaminaAttributeSet()
+{
+	InitStamina(100.f);
+	InitMaxStamina(100.f);
+	InitStaminaRegen(25.f);
+}
+
+void UStaminaAttributeSet::ClampAttribute(const FGameplayAttribute& Attribute, float& NewValue) const
+{
+	if (Attribute == GetStaminaAttribute())
+	{
+		// clamp to Max
+		NewValue = FMath::Clamp(NewValue, 0.f, GetMaxStamina());
+	}
+	else if (Attribute == GetMaxStaminaAttribute())
+	{
+		// always keep Max rounded and >= 1
+		NewValue = FMath::RoundFromZero(FMath::Max(NewValue, 1.f));
+	}
+}
+
+void UStaminaAttributeSet::PreAttributeBaseChange(const FGameplayAttribute& Attribute, float& NewValue) const
+{
+	// apply clamping to base values
+	ClampAttribute(Attribute, NewValue);
+}
+
+void UStaminaAttributeSet::PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue)
+{
+	// also apply clamping to current values (changed by temporary modifiers)
+	ClampAttribute(Attribute, NewValue);
+}
+
+void UStaminaAttributeSet::PostAttributeChange(const FGameplayAttribute& Attribute, float OldValue, float NewValue)
+{
+	if (Attribute == GetMaxStaminaAttribute())
+	{
+		// just clamp down, don't increase proportionally
+		if (NewValue < GetStamina())
+		{
+			SetStamina(NewValue);
+		}
+	}
+}
+
+void UStaminaAttributeSet::OnRep_Stamina(FGameplayAttributeData& OldValue)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UStaminaAttributeSet, Stamina, OldValue);
+}
+
+void UStaminaAttributeSet::OnRep_MaxStamina(FGameplayAttributeData& OldValue)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UStaminaAttributeSet, MaxStamina, OldValue);
+}
+
+void UStaminaAttributeSet::OnRep_StaminaRegen(FGameplayAttributeData& OldValue)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UStaminaAttributeSet, StaminaRegen, OldValue);
+}
+
+
+void UStaminaAttributeSet::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME_CONDITION_NOTIFY(UStaminaAttributeSet, Stamina, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UStaminaAttributeSet, MaxStamina, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UStaminaAttributeSet, StaminaRegen, COND_None, REPNOTIFY_Always);
+}
---
+++ Source/AbilitiesSample/Abilities/StaminaAttributeSet.h
@@ -0,0 +1,49 @@
+﻿// Copyright Bohdon Sayre, All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AttributeSet.h"
+#include "ExtendedGameplayAttributeAccessors.h"
+#include "StaminaAttributeSet.generated.h"
+
+
+/**
+ * Attribute set for Stamina
+ */
+UCLASS()
+class ABILITIESSAMPLE_API UStaminaAttributeSet : public UAttributeSet
+{
+	GENERATED_BODY()
+
+public:
+	UStaminaAttributeSet();
+
+	/** How much Stamina to regenerate each second. */
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Stamina, Category = "Stamina")
+	FGameplayAttributeData Stamina;
+	GAMEPLAYATTRIBUTE_ACCESSORS(UStaminaAttributeSet, Stamina)
+	UFUNCTION()
+	virtual void OnRep_Stamina(FGameplayAttributeData& OldValue);
+
+	/** How much Stamina to regenerate each second. */
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_MaxStamina, Category = "Stamina")
+	FGameplayAttributeData MaxStamina;
+	GAMEPLAYATTRIBUTE_ACCESSORS(UStaminaAttributeSet, MaxStamina)
+	UFUNCTION()
+	virtual void OnRep_MaxStamina(FGameplayAttributeData& OldValue);
+
+	/** How much Stamina to regenerate each second. */
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_StaminaRegen, Category = "Stamina")
+	FGameplayAttributeData StaminaRegen;
+	GAMEPLAYATTRIBUTE_ACCESSORS(UStaminaAttributeSet, StaminaRegen)
+	UFUNCTION()
+	virtual void OnRep_StaminaRegen(FGameplayAttributeData& OldValue);
+
+	/** Perform clamping specific to this attribute set. For use when either base or current value is changing. */
+	virtual void ClampAttribute(const FGameplayAttribute& Attribute, float& NewValue) const;
+
+	virtual void PreAttributeBaseChange(const FGameplayAttribute& Attribute, float& NewValue) const override;
+	virtual void PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue) override;
+	virtual void PostAttributeChange(const FGameplayAttribute& Attribute, float OldValue, float NewValue) override;
+};
