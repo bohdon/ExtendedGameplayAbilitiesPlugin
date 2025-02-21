5854a383e74979f227a1b88890fa751293598f09
Bohdon Sayre <bohdon@gmail.com>
2025-02-20 23:22:36 -0500

    add sample effect ui data and display them in the hud

---
+++ Source/AbilitiesSample/UI/USampleGameplayEffectUIData.h
@@ -0,0 +1,21 @@
+﻿// Copyright Bohdon Sayre, All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayEffectUIData_TextOnly.h"
+#include "USampleGameplayEffectUIData.generated.h"
+
+
+/**
+ * Gameplay effect UI data for the project.
+ */
+UCLASS(DisplayName="UI Data (Abilities Sample)")
+class ABILITIESSAMPLE_API UUSampleGameplayEffectUIData : public UGameplayEffectUIData_TextOnly
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = Data)
+	FLinearColor Color = FLinearColor::White;
+};
