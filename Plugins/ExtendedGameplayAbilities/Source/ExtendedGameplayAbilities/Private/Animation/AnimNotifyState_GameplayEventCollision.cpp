2b986562fb0a213f2af2bdf8859e3e3a9bd81005
Bohdon Sayre <bohdon@gmail.com>
2025-02-23 17:03:23 -0500

    add gameplay event collision anim notify state for setting up anim based shape collisions

---
+++ Plugins/ExtendedGameplayAbilities/Source/ExtendedGameplayAbilities/Private/Animation/AnimNotifyState_GameplayEventCollision.cpp
@@ -0,0 +1,238 @@
+﻿// Copyright Bohdon Sayre, All Rights Reserved.
+
+
+#include "Animation/AnimNotifyState_GameplayEventCollision.h"
+
+#include "AbilitySystemComponent.h"
+#include "AbilitySystemGlobals.h"
+#include "Components/BoxComponent.h"
+#include "Components/CapsuleComponent.h"
+#include "Components/SkeletalMeshComponent.h"
+#include "Components/SphereComponent.h"
+
+
+void UAnimNotifyState_GameplayEventCollision::NotifyBegin(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, float TotalDuration,
+                                                          const FAnimNotifyEventReference& EventReference)
+{
+	Super::NotifyBegin(MeshComp, Animation, TotalDuration, EventReference);
+
+	if (UPrimitiveComponent* CollisionComp = SpawnCollision(MeshComp, Animation))
+	{
+		// tag the component so it can be cleaned up
+		CollisionComp->ComponentTags.AddUnique(GetSpawnedComponentTag());
+	}
+}
+
+void UAnimNotifyState_GameplayEventCollision::NotifyEnd(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation,
+                                                        const FAnimNotifyEventReference& EventReference)
+{
+	Super::NotifyEnd(MeshComp, Animation, EventReference);
+
+	if (UPrimitiveComponent* PrimitiveComp = GetSpawnedCollision(MeshComp))
+	{
+		PrimitiveComp->DestroyComponent();
+	}
+}
+
+FString UAnimNotifyState_GameplayEventCollision::GetNotifyName_Implementation() const
+{
+	return FString::Printf(TEXT("%s Collision -> %s"),
+	                       *StaticEnum<EGameplayEventCollisionShapeType>()->GetNameStringByValue(static_cast<int64>(ShapeType)),
+	                       *BeginOverlapEventTag.ToString());
+}
+
+float UAnimNotifyState_GameplayEventCollision::GetShapeRadius() const
+{
+	return DefaultRadius;
+}
+
+float UAnimNotifyState_GameplayEventCollision::GetShapeCapsuleHalfHeight() const
+{
+	return DefaultCapsuleHalfHeight;
+}
+
+FVector UAnimNotifyState_GameplayEventCollision::GetShapeBoxHalfExtents() const
+{
+	return DefaultBoxHalfExtents;
+}
+
+UPrimitiveComponent* UAnimNotifyState_GameplayEventCollision::SpawnCollision(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation) const
+{
+	TSubclassOf<UPrimitiveComponent> ComponentClass;
+	switch (ShapeType)
+	{
+	case EGameplayEventCollisionShapeType::Sphere:
+		ComponentClass = USphereComponent::StaticClass();
+		break;
+	case EGameplayEventCollisionShapeType::Capsule:
+		ComponentClass = UCapsuleComponent::StaticClass();
+		break;
+	case EGameplayEventCollisionShapeType::Box:
+		ComponentClass = UBoxComponent::StaticClass();
+		break;
+	}
+
+	if (UPrimitiveComponent* CollisionComp = NewObject<UPrimitiveComponent>(MeshComp, ComponentClass, NAME_None, RF_Transient))
+	{
+		CollisionComp->SetCollisionProfileName(CollisionProfileName.Name);
+		CollisionComp->SetupAttachment(MeshComp, SocketName);
+		CollisionComp->SetRelativeLocationAndRotation(Location, Rotation);
+
+		switch (ShapeType)
+		{
+		case EGameplayEventCollisionShapeType::Sphere:
+			if (USphereComponent* SphereComp = Cast<USphereComponent>(CollisionComp))
+			{
+				SphereComp->SetSphereRadius(GetShapeRadius(), false);
+			}
+			break;
+		case EGameplayEventCollisionShapeType::Capsule:
+			if (UCapsuleComponent* CapsuleComp = Cast<UCapsuleComponent>(CollisionComp))
+			{
+				CapsuleComp->SetCapsuleSize(GetShapeRadius(), GetShapeCapsuleHalfHeight(), false);
+			}
+			break;
+		case EGameplayEventCollisionShapeType::Box:
+			if (UBoxComponent* BoxComponent = Cast<UBoxComponent>(CollisionComp))
+			{
+				BoxComponent->SetBoxExtent(GetShapeBoxHalfExtents(), false);
+			}
+			break;
+		}
+
+		if (BeginOverlapEventTag.IsValid())
+		{
+			CollisionComp->OnComponentBeginOverlap.AddDynamic(this, &UAnimNotifyState_GameplayEventCollision::OnBeginOverlap);
+		}
+		if (EndOverlapEventTag.IsValid())
+		{
+			CollisionComp->OnComponentEndOverlap.AddDynamic(this, &UAnimNotifyState_GameplayEventCollision::OnEndOverlap);
+		}
+
+		CollisionComp->RegisterComponent();
+
+		return CollisionComp;
+	}
+	return nullptr;
+}
+
+UPrimitiveComponent* UAnimNotifyState_GameplayEventCollision::GetSpawnedCollision(UMeshComponent* MeshComp) const
+{
+	if (!MeshComp)
+	{
+		return nullptr;
+	}
+
+	TArray<USceneComponent*> Children;
+	MeshComp->GetChildrenComponents(false, Children);
+
+	const FName ComponentTag = GetSpawnedComponentTag();
+	for (USceneComponent* Component : Children)
+	{
+		if (Component && Component->ComponentHasTag(ComponentTag))
+		{
+			if (UPrimitiveComponent* PrimitiveComp = CastChecked<UPrimitiveComponent>(Component))
+			{
+				return PrimitiveComp;
+			}
+		}
+	}
+	return nullptr;
+}
+
+bool UAnimNotifyState_GameplayEventCollision::ShouldIgnoreOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+                                                                  UPrimitiveComponent* OtherComp, int32 OtherBodyIndex) const
+{
+	if (bIgnoreSelf && OtherActor == OverlappedComponent->GetOwner())
+	{
+		return true;
+	}
+	return false;
+}
+
+void UAnimNotifyState_GameplayEventCollision::OnBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp,
+                                                             int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (ShouldIgnoreOverlap(OverlappedComponent, OtherActor, OtherComp, OtherBodyIndex))
+	{
+		return;
+	}
+
+	AActor* OwningActor = OverlappedComponent->GetOwner();
+	if (UAbilitySystemComponent* AbilitySystem = UAbilitySystemGlobals::GetAbilitySystemComponentFromActor(OwningActor))
+	{
+		FGameplayEventData Payload;
+		Payload.EventTag = BeginOverlapEventTag;
+		Payload.Instigator = OwningActor;
+		Payload.Target = AbilitySystem->GetAvatarActor();
+
+		// create target data from the hit result
+		/** Note: These are cleaned up by the FGameplayAbilityTargetDataHandle (via an internal TSharedPtr) */
+		FGameplayAbilityTargetData_SingleTargetHit* ReturnData = new FGameplayAbilityTargetData_SingleTargetHit();
+		ReturnData->HitResult = SweepResult;
+		Payload.TargetData.Add(ReturnData);
+
+		FScopedPredictionWindow NewScopedWindow(AbilitySystem, true);
+		AbilitySystem->HandleGameplayEvent(Payload.EventTag, &Payload);
+	}
+}
+
+void UAnimNotifyState_GameplayEventCollision::OnEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp,
+                                                           int32 OtherBodyIndex)
+{
+	if (ShouldIgnoreOverlap(OverlappedComponent, OtherActor, OtherComp, OtherBodyIndex))
+	{
+		return;
+	}
+
+	AActor* OwningActor = OverlappedComponent->GetOwner();
+	if (UAbilitySystemComponent* AbilitySystem = UAbilitySystemGlobals::GetAbilitySystemComponentFromActor(OwningActor))
+	{
+		FGameplayEventData Payload;
+		Payload.EventTag = EndOverlapEventTag;
+		Payload.Instigator = OwningActor;
+		Payload.Target = AbilitySystem->GetAvatarActor();
+
+		// make a fake hit result for the target data
+		FHitResult HitResult;
+		HitResult.HitObjectHandle = FActorInstanceHandle(OtherActor);
+		HitResult.Component = OtherComp;
+
+		// create target data from the hit result
+		/** Note: These are cleaned up by the FGameplayAbilityTargetDataHandle (via an internal TSharedPtr) */
+		FGameplayAbilityTargetData_SingleTargetHit* ReturnData = new FGameplayAbilityTargetData_SingleTargetHit();
+		ReturnData->HitResult = HitResult;
+		Payload.TargetData.Add(ReturnData);
+
+		FScopedPredictionWindow NewScopedWindow(AbilitySystem, true);
+		AbilitySystem->HandleGameplayEvent(Payload.EventTag, &Payload);
+	}
+}
+
+#if WITH_EDITOR
+bool UAnimNotifyState_GameplayEventCollision::CanEditChange(const FProperty* InProperty) const
+{
+	const bool bCanEdit = Super::CanEditChange(InProperty);
+
+	if (bCanEdit && InProperty)
+	{
+		const FName PropertyName = InProperty->GetFName();
+		if (PropertyName == GET_MEMBER_NAME_CHECKED(UAnimNotifyState_GameplayEventCollision, DefaultRadius))
+		{
+			return ShapeType == EGameplayEventCollisionShapeType::Sphere || ShapeType == EGameplayEventCollisionShapeType::Capsule;
+		}
+
+		if (PropertyName == GET_MEMBER_NAME_CHECKED(UAnimNotifyState_GameplayEventCollision, DefaultCapsuleHalfHeight))
+		{
+			return ShapeType == EGameplayEventCollisionShapeType::Capsule;
+		}
+
+		if (PropertyName == GET_MEMBER_NAME_CHECKED(UAnimNotifyState_GameplayEventCollision, DefaultBoxHalfExtents))
+		{
+			return ShapeType == EGameplayEventCollisionShapeType::Box;
+		}
+	}
+
+	return true;
+}
+#endif
---
+++ Plugins/ExtendedGameplayAbilities/Source/ExtendedGameplayAbilities/Public/Animation/AnimNotifyState_GameplayEventCollision.h
@@ -0,0 +1,110 @@
+﻿// Copyright Bohdon Sayre, All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+#include "Animation/AnimNotifies/AnimNotifyState.h"
+#include "Engine/CollisionProfile.h"
+#include "Engine/HitResult.h"
+#include "AnimNotifyState_GameplayEventCollision.generated.h"
+
+class UMeshComponent;
+class UPrimitiveComponent;
+class USkeletalMeshComponent;
+
+
+UENUM(BlueprintType)
+enum class EGameplayEventCollisionShapeType : uint8
+{
+	Sphere,
+	Capsule,
+	Box,
+};
+
+
+/**
+ * Adds a collision component to a bone or socket, and triggers
+ * a gameplay event when on overlap.
+ */
+UCLASS(Const, HideCategories = Object, CollapseCategories, Meta = (DisplayName = "Gameplay Event Collision"))
+class EXTENDEDGAMEPLAYABILITIES_API UAnimNotifyState_GameplayEventCollision : public UAnimNotifyState
+{
+	GENERATED_BODY()
+
+public:
+	/** The gameplay event tag to send on overlap begin. */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Collision")
+	FGameplayTag BeginOverlapEventTag;
+
+	/** The gameplay event tag to send on overlap end. */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Collision")
+	FGameplayTag EndOverlapEventTag;
+
+	/** The type of shape to create. */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Collision")
+	EGameplayEventCollisionShapeType ShapeType;
+
+	/** The collision profile name to use for the shape. */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Collision")
+	FCollisionProfileName CollisionProfileName;
+
+	/** The socket or bone to attach to. */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Collision")
+	FName SocketName;
+
+	/** The relative location of the collision shape. */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Collision")
+	FVector Location;
+
+	/** The relative rotation of the collision shape. */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Collision")
+	FRotator Rotation;
+
+	/** The default shape radius when the shape is set to Sphere or Capsule */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Collision")
+	float DefaultRadius = 10.0f;
+
+	/** The default capsule half height when the shape is set to Capsule */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Collision")
+	float DefaultCapsuleHalfHeight = 10.0f;
+
+	/** The default extents when the shape is set to Box */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Collision")
+	FVector DefaultBoxHalfExtents = FVector(10.0f, 10.f, 10.f);
+
+	/** Ignore collisions with the owning actor. */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Collision")
+	bool bIgnoreSelf = true;
+
+	virtual void NotifyBegin(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, float TotalDuration,
+	                         const FAnimNotifyEventReference& EventReference) override;
+	virtual void NotifyEnd(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation,
+	                       const FAnimNotifyEventReference& EventReference) override;
+	virtual FString GetNotifyName_Implementation() const override;
+
+	UFUNCTION()
+	void OnBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex,
+	                    bool bFromSweep, const FHitResult& SweepResult);
+	UFUNCTION()
+	void OnEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
+#if WITH_EDITOR
+	virtual bool CanEditChange(const FProperty* InProperty) const override;
+#endif
+
+protected:
+	virtual float GetShapeRadius() const;
+	virtual float GetShapeCapsuleHalfHeight() const;
+	virtual FVector GetShapeBoxHalfExtents() const;
+
+	/** Spawn the collision component. */
+	virtual UPrimitiveComponent* SpawnCollision(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation) const;
+
+	/** Return the spawned collision component for a mesh. */
+	virtual UPrimitiveComponent* GetSpawnedCollision(UMeshComponent* MeshComp) const;
+
+	FORCEINLINE FName GetSpawnedComponentTag() const { return GetFName(); }
+
+	virtual bool ShouldIgnoreOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex) const;
+};
