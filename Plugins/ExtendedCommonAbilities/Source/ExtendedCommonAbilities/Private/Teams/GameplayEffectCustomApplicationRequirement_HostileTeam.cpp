630b1e266269b5138d439a32d9188d45bd559897
Bohdon Sayre <bohdon@gmail.com>
2025-03-01 17:18:44 -0500

    add custom effect application requirement class that checks for hostile teams

---
+++ Plugins/ExtendedCommonAbilities/Source/ExtendedCommonAbilities/Private/Teams/GameplayEffectCustomApplicationRequirement_HostileTeam.cpp
@@ -0,0 +1,23 @@
+﻿// Copyright Bohdon Sayre, All Rights Reserved.
+
+
+#include "Teams/GameplayEffectCustomApplicationRequirement_HostileTeam.h"
+
+#include "AbilitySystemComponent.h"
+#include "Teams/CommonTeamsComponent.h"
+#include "Teams/CommonTeamStatics.h"
+
+
+bool UGameplayEffectCustomApplicationRequirement_HostileTeam::CanApplyGameplayEffect_Implementation(const UGameplayEffect* GameplayEffect,
+                                                                                                    const FGameplayEffectSpec& Spec,
+                                                                                                    UAbilitySystemComponent* ASC) const
+{
+	if (const UCommonTeamsComponent* TeamsComp = UCommonTeamStatics::GetTeamsComponent(ASC))
+	{
+		if (const AActor* Instigator = Spec.GetEffectContext().GetInstigator())
+		{
+			return TeamsComp->GetAttitude(Instigator, ASC->GetOwner()) == ETeamAttitude::Hostile;
+		}
+	}
+	return true;
+}
