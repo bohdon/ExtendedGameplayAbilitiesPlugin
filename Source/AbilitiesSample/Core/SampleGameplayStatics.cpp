601ff17daece11afc3cd21865ac15937c63ebcb9
Bohdon Sayre <bohdon@gmail.com>
2025-04-01 22:50:36 -0400

    fix unit status team attitude not being accurate, and use team change events to make sure it updates

---
+++ Source/AbilitiesSample/Core/SampleGameplayStatics.cpp
@@ -0,0 +1,21 @@
+﻿// Copyright Bohdon Sayre, All Rights Reserved.
+
+
+#include "SampleGameplayStatics.h"
+
+#include "Engine/Engine.h"
+#include "Engine/LocalPlayer.h"
+
+
+APlayerController* USampleGameplayStatics::GetPlayerControllerFromLocalPlayer(const UObject* WorldContextObject, const ULocalPlayer* LocalPlayer)
+{
+	if (!LocalPlayer)
+	{
+		return nullptr;
+	}
+	if (const UWorld* World = GEngine->GetWorldFromContextObject(WorldContextObject, EGetWorldErrorMode::LogAndReturnNull))
+	{
+		return LocalPlayer->GetPlayerController(World);
+	}
+	return nullptr;
+}
---
+++ Source/AbilitiesSample/Core/SampleGameplayStatics.h
@@ -0,0 +1,24 @@
+﻿// Copyright Bohdon Sayre, All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "SampleGameplayStatics.generated.h"
+
+class ULocalPlayer;
+
+
+/**
+ * Some static functions for the sample project.
+ */
+UCLASS()
+class ABILITIESSAMPLE_API USampleGameplayStatics : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+public:
+	/** Return the PlayerController for a local player. */
+	UFUNCTION(BlueprintPure, Category = "Game", meta = (WorldContext = "WorldContextObject"))
+	static APlayerController* GetPlayerControllerFromLocalPlayer(const UObject* WorldContextObject, const ULocalPlayer* LocalPlayer);
+};
