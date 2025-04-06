0dc57c5aa9b7fd687dc6e373f0316b0081f77959
Bohdon Sayre <bohdon@gmail.com>
2025-04-06 15:34:36 -0400

    add AbilitiesAIController that pulls ability system from player state and implements tag interface

---
+++ Plugins/ExtendedCommonAbilities/Source/ExtendedCommonAbilities/Private/AbilitiesAIController.cpp
@@ -0,0 +1,59 @@
+﻿// Copyright Bohdon Sayre. All Rights Reserved.
+
+
+#include "AbilitiesAIController.h"
+
+#include "AbilitySystemComponent.h"
+#include "AbilitySystemGlobals.h"
+#include "GameFramework/PlayerState.h"
+
+
+AAbilitiesAIController::AAbilitiesAIController()
+{
+	PrimaryActorTick.bCanEverTick = true;
+	bWantsPlayerState = true;
+}
+
+UAbilitySystemComponent* AAbilitiesAIController::GetAbilitySystemComponent() const
+{
+	return UAbilitySystemGlobals::GetAbilitySystemComponentFromActor(PlayerState);
+}
+
+void AAbilitiesAIController::GetOwnedGameplayTags(FGameplayTagContainer& TagContainer) const
+{
+	if (const UAbilitySystemComponent* AbilitySystem = GetAbilitySystemComponent())
+	{
+		AbilitySystem->GetOwnedGameplayTags(TagContainer);
+	}
+	else
+	{
+		TagContainer.Reset();
+	}
+}
+
+bool AAbilitiesAIController::HasMatchingGameplayTag(FGameplayTag TagToCheck) const
+{
+	if (const UAbilitySystemComponent* AbilitySystem = GetAbilitySystemComponent())
+	{
+		return AbilitySystem->HasMatchingGameplayTag(TagToCheck);
+	}
+	return false;
+}
+
+bool AAbilitiesAIController::HasAllMatchingGameplayTags(const FGameplayTagContainer& TagContainer) const
+{
+	if (const UAbilitySystemComponent* AbilitySystem = GetAbilitySystemComponent())
+	{
+		return AbilitySystem->HasAllMatchingGameplayTags(TagContainer);
+	}
+	return false;
+}
+
+bool AAbilitiesAIController::HasAnyMatchingGameplayTags(const FGameplayTagContainer& TagContainer) const
+{
+	if (const UAbilitySystemComponent* AbilitySystem = GetAbilitySystemComponent())
+	{
+		return AbilitySystem->HasAnyMatchingGameplayTags(TagContainer);
+	}
+	return false;
+}
---
+++ Plugins/ExtendedCommonAbilities/Source/ExtendedCommonAbilities/Public/AbilitiesAIController.h
@@ -0,0 +1,34 @@
+﻿// Copyright Bohdon Sayre. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystemInterface.h"
+#include "AIController.h"
+#include "GameplayTagAssetInterface.h"
+#include "AbilitiesAIController.generated.h"
+
+
+/**
+ * An AIController designed for use with an ability system on the player state.
+ * Implements the ability system and gameplay tag interfaces.
+ */
+UCLASS()
+class EXTENDEDCOMMONABILITIES_API AAbilitiesAIController : public AAIController,
+                                                           public IAbilitySystemInterface,
+                                                           public IGameplayTagAssetInterface
+{
+	GENERATED_BODY()
+
+public:
+	AAbilitiesAIController();
+
+	// IAbilitySystemInterface
+	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override;
+
+	// IGameplayTagAssetInterface
+	virtual void GetOwnedGameplayTags(FGameplayTagContainer& TagContainer) const override;
+	virtual bool HasMatchingGameplayTag(FGameplayTag TagToCheck) const override;
+	virtual bool HasAllMatchingGameplayTags(const FGameplayTagContainer& TagContainer) const override;
+	virtual bool HasAnyMatchingGameplayTags(const FGameplayTagContainer& TagContainer) const override;
+};
